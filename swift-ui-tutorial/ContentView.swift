207864b62eba346cc34f3c721fa6e09a02f57823
Emir Seyhan <emirseyhana.z@gmail.com>
2023-10-21 15:36:14 +0300

    First commit

--- swift-ui-tutorial/ContentView.swift
+++ swift-ui-tutorial/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            MapView().frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("Taşa benzer yüreğim")
+                    .font(.title2)
+                Text("Taş hakkında çok detaylı bilgiler")
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
